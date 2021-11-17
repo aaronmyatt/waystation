0146dba398156749541cf565ef64262be5eb7c1f
atm <aaronmyatt@gmail.com>
2021-11-17 13:43:20 +0800

    shows user message if !hasMarks

--- commands/listCommand.ts
+++ commands/listCommand.ts
@@ -1,7 +1,7 @@
 import { Command } from "https://deno.land/x/cliffy@v0.20.0/command/mod.ts";
 
 import { readWaystationFromFS as readWaystation } from "../utils/mod.ts";
-import { markSelector } from "../components/mod.ts";
+import { markSelector  } from "../components/mod.ts";
 
 export default function listCommand() {
   return new Command()
@@ -10,7 +10,13 @@ export default function listCommand() {
     )
     .action(async () => {
       const waystation = await readWaystation();
-      while (true) {
+      const hasMarks = waystation.marks.length > 0;
+      if(!hasMarks){
+        console.log("No marks found.")
+        console.log("Use: waystion m '/a/path/of/interst:1'")
+        console.log("To add new marks to the current Waystation")
+      }
+      while (hasMarks) {
         await markSelector(waystation);
       }
     });
