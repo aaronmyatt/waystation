25375c7ad1727714e72f56afe5687c4579cf3f57
atm <aaronmyatt@gmail.com>
2021-11-19 23:01:35 +0800

    open command action implementation

--- commands/openCommand.ts
+++ commands/openCommand.ts
@@ -1,4 +1,6 @@
 import { Command } from "https://deno.land/x/cliffy@v0.20.0/command/mod.ts";
+import { readRecentWaystations, readWaystationFromFS, writeCurrentToFS, writeBackupToFS } from '../utils/mod.ts';
+import { stationSelector } from '../components/mod.ts';
 
 export default function openCommand() {
   return new Command()
@@ -6,5 +8,13 @@ export default function openCommand() {
       "Open previous waystation",
     )
     .action(async () => {
+      const waystation = await readWaystationFromFS();
+      const backups = await readRecentWaystations(undefined);
+      const newWaystation = await stationSelector(backups);
+      if(newWaystation){
+        writeCurrentToFS(waystation);
+        console.log('Updated current active Waystation');
+      }
+      writeBackupToFS(waystation);
     });
 }
