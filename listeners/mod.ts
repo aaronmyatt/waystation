1f10d4eaa3b1a12f87bba270bb025e3238ca6020
atm <aaronmyatt@gmail.com>
2021-11-29 22:04:31 +0800

    crud event handles

--- listeners/mod.ts
+++ listeners/mod.ts
@@ -1,14 +1,58 @@
 import { events } from "../core/constants.ts";
 import { writeBackupToFS, writeCurrentToFS } from "../utils/mod.ts";
 
-function writeWaystationOnEdit(){
-    addEventListener(events.EDIT_MARK, function (e) {
-        const waystation = (e as CustomEvent).detail.waystation;
-        writeCurrentToFS(waystation);
-        writeBackupToFS(waystation);
-    });
+const _writeAndBackup = (waystation: IWaystation) => {
+  writeCurrentToFS(waystation);
+  writeBackupToFS(waystation);
+};
+
+function onEditMark() {
+  addEventListener(events.EDIT_MARK, function (e) {
+    const waystation = (e as CustomEvent).detail.waystation;
+    _writeAndBackup(waystation);
+  });
+}
+
+function onNewMark() {
+  addEventListener(events.NEW_MARK, function (e) {
+    const waystation = (e as CustomEvent).detail.waystation;
+    _writeAndBackup(waystation);
+  });
+}
+
+function onEditResource() {
+  addEventListener(events.EDIT_RESOURCE, function (e) {
+    const waystation = (e as CustomEvent).detail.waystation;
+    _writeAndBackup(waystation);
+  });
+}
+
+function onNewResource() {
+  addEventListener(events.NEW_RESOURCE, function (e) {
+    const waystation = (e as CustomEvent).detail.waystation;
+    _writeAndBackup(waystation);
+  });
+}
+
+function onNewWaystation() {
+  addEventListener(events.NEW_WAYSTATION, function (e) {
+    const waystation = (e as CustomEvent).detail.waystation;
+    console.log(waystation);
+  });
+}
+
+function onEditWaystation() {
+  addEventListener(events.EDIT_WAYSTATION, function (e) {
+    const waystation = (e as CustomEvent).detail.waystation;
+    console.log(waystation);
+  });
 }
 
 export default function registerListeners() {
-  writeWaystationOnEdit();
+  onEditMark();
+  onNewMark();
+  onEditResource();
+  onNewResource();
+  onNewWaystation();
+  onEditWaystation
 }
