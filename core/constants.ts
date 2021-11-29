0863d31922a9cbf32e030a8d730ef6b3f9abfa12
atm <aaronmyatt@gmail.com>
2021-11-29 22:01:25 +0800

    add more crud event names

--- core/constants.ts
+++ core/constants.ts
@@ -1,11 +1,15 @@
 export const events = {
-    EDIT_MARK: "waystation:mark:edit",
-    NEW_WAYSTATION: "waystation:new",
+  EDIT_MARK: "waystation:mark:edit",
+  NEW_MARK: "waystation:mark:new",
+  EDIT_RESOURCE: "waystation:resource:edit",
+  NEW_RESOURCE: "waystation:resource:new",
+  EDIT_WAYSTATION: "waystation:edit",
+  NEW_WAYSTATION: "waystation:new",
 };
 
 export const paths = {};
 
 export default {
-    events,
-    paths
-}
+  events,
+  paths,
+};
