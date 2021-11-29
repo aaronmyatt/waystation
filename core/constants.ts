416a6d938406d4680ca6b397e30af783669f392e
atm <aaronmyatt@gmail.com>
2021-11-29 16:37:28 +0800

    extract constants to module

---
+++ core/constants.ts
@@ -0,0 +1,11 @@
+export const events = {
+    EDIT_MARK: "waystation:mark:edit",
+    NEW_WAYSTATION: "waystation:new",
+};
+
+export const paths = {};
+
+export default {
+    events,
+    paths
+}
