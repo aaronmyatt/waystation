f70fe28bb824de5e65da75e9296cc48099b8f22a
atm <aaronmyatt@gmail.com>
2021-12-01 15:09:35 +0800

    make mark paths full/absolute asynchronously

--- listeners/mod.ts
+++ listeners/mod.ts
@@ -1,7 +1,16 @@
+import * as pathLib from "https://deno.land/std@0.113.0/path/mod.ts";
 import Waystation from "../core/waystation.ts";
 import { events } from "../core/constants.ts";
 import { writeBackupToFS, writeCurrentToFS } from "../utils/mod.ts";
 import fileContextResource from "../fileContextResource.ts";
+
+const _fullPath = (filename: string) => pathLib.join(Deno.cwd(), filename);
+
+const ensureMarkPathAbsolute = (mark: IMark) => {
+  if (pathLib.isAbsolute(mark.path)) return mark;
+  mark.path = _fullPath(mark.path || "");
+  return mark;
+};
 
 const _writeAndBackup = async (waystation: IWaystation) => {
   await writeCurrentToFS(waystation);
@@ -9,18 +18,23 @@ const _writeAndBackup = async (waystation: IWaystation) => {
 };
 
 function onEditMark() {
-  addEventListener(events.EDIT_MARK, function (e) {
+  addEventListener(events.EDIT_MARK, async function (e) {
     let { waystation, mark } = (e as CustomEvent).detail;
-    waystation = Waystation.removeResourceByName(waystation, mark, "File Context")
-    fileContextResource(waystation, mark).then(waystation => {
-      _writeAndBackup(waystation);
-    });
+    waystation = Waystation.removeResourceByName(
+      waystation,
+      mark,
+      "File Context",
+    );
+    waystation = await fileContextResource(waystation, mark);
+    waystation.marks = waystation.marks.map(ensureMarkPathAbsolute);
+    _writeAndBackup(waystation);
   });
 }
 
 function onNewMark() {
   addEventListener(events.NEW_MARK, function (e) {
     const waystation = (e as CustomEvent).detail.waystation;
+    waystation.marks = waystation.marks.map(ensureMarkPathAbsolute);
     _writeAndBackup(waystation);
   });
 }
